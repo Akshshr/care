180aad99f205c8e665875dcfa5b2471fefa13baa
Angel Kavazov <elenarie@live.com>
2015-06-21 20:11:38 +0200

    Added the basic Windows project.

---
+++ Care.Windows/Care.Windows/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Care")]
+[assembly: AssemblyProduct("Care")]
+[assembly: AssemblyDescription("Care")]
+[assembly: AssemblyCompany("Care")]
+[assembly: AssemblyCopyright("© Care")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+[assembly: ComVisible(false)]
